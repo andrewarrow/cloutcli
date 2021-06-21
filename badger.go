aff5934a3dfbcee388c69d91cb9dfb592d3e6ecb
Andrew Arrow <oneone@gmail.com>
2021-06-21 09:52:55 -0700

    moving badger loop to use channel

--- badger.go
+++ badger.go
@@ -1,11 +1,22 @@
 package cloutcli
 
 import (
+	"fmt"
+
 	"github.com/andrewarrow/cloutcli/database"
+	"github.com/andrewarrow/cloutcli/lib"
 	"github.com/dgraph-io/badger/v3"
 )
 
+// magic numbers are defined in
+// https://github.com/bitclout/core/blob/main/lib/db_utils.go
+
 func ImportFromBadgerToSqlite(dir string) error {
+	db, err := badger.Open(badger.DefaultOptions(dir))
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 	return nil
 }
 
@@ -15,8 +26,10 @@ func PrintAllPostsFromBadger(dir string) error {
 		return err
 	}
 	defer db.Close()
-	// magic numbers are defined in
-	// https://github.com/bitclout/core/blob/main/lib/db_utils.go
-	database.EnumeratePosts(db, []byte{17})
+	postEntryChan := make(chan *lib.PostEntry, 1024)
+	go database.EnumeratePosts(db, &postEntryChan)
+	for postEntry := range postEntryChan {
+		fmt.Println(string(postEntry.Body))
+	}
 	return nil
 }
--- database/posts.go
+++ database/posts.go
@@ -3,25 +3,25 @@ package database
 import (
 	"bytes"
 	"encoding/gob"
-	"fmt"
 
 	"github.com/andrewarrow/cloutcli/lib"
 	"github.com/dgraph-io/badger/v3"
 )
 
-func EnumeratePosts(db *badger.DB, prefix []byte) {
+func EnumeratePosts(db *badger.DB, c *chan *lib.PostEntry) {
 
 	db.View(func(txn *badger.Txn) error {
 		opts := badger.DefaultIteratorOptions
 		nodeIterator := txn.NewIterator(opts)
 		defer nodeIterator.Close()
+		prefix := []byte{17}
 
 		for nodeIterator.Seek(prefix); nodeIterator.ValidForPrefix(prefix); nodeIterator.Next() {
 			val, _ := nodeIterator.Item().ValueCopy(nil)
 
 			post := &lib.PostEntry{}
 			gob.NewDecoder(bytes.NewReader(val)).Decode(post)
-			fmt.Println(string(post.Body))
+			*c <- post
 		}
 		return nil
 	})
