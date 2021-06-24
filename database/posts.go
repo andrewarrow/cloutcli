f4410bea2cae8037b84729d02997c04019e608d7
Andrew Arrow <oneone@gmail.com>
2021-06-24 09:02:40 -0700

    looping through every post

--- database/posts.go
+++ database/posts.go
@@ -14,20 +14,30 @@ import (
 func PostsByAuthor(sdb *sql.DB, db *badger.DB, author string) {
 
 	postMap := map[string]bool{}
-	prefix := []byte{35}
-	prefix = append(prefix, UsernameToPub(db, author)...)
+	prefix := []byte{17}
+	goal := base58.Encode(UsernameToPub(db, author))
+	//prefix = append(prefix, UsernameToPub(db, author)...)
 	db.View(func(txn *badger.Txn) error {
 		opts := badger.DefaultIteratorOptions
 		it := txn.NewIterator(opts)
 		defer it.Close()
 
-		timestampSizeBytes := 8
+		i := 0
 		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
-			rawKey := it.Item().Key()
-			keyWithoutPrefix := rawKey[1:]
-			publicKeySizeBytes := lib.HashSizeBytes + 1
-			hash := keyWithoutPrefix[(publicKeySizeBytes + timestampSizeBytes):]
-			postMap[base58.Encode(hash)] = true
+			if i%1000 == 0 {
+				fmt.Println("iteration", i)
+			}
+			val, _ := it.Item().ValueCopy(nil)
+
+			post := &lib.PostEntry{}
+			gob.NewDecoder(bytes.NewReader(val)).Decode(post)
+
+			ppk := base58.Encode(post.PosterPublicKey)
+			if ppk == goal {
+				postMap[base58.Encode(post.PostHash.Bytes())] = true
+			}
+
+			i++
 		}
 		return nil
 	})
