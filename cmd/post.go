34077ea51dc9492d3cc1612a42ecf07a378e2b3b
Andrew Arrow <oneone@gmail.com>
2021-07-07 11:06:54 -0700

    handle one post - inspect its data

--- cmd/post.go
+++ cmd/post.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/andrewarrow/cloutcli"
 )
 
 func PrintPostHelp() {
@@ -18,9 +21,17 @@ func HandlePosts() {
 	command := os.Args[2]
 	if command == "inspect" {
 		payload := os.Args[3]
-		fmt.Println(payload)
+		HandleOnePost(payload)
 	}
 }
 
-func HandleOnePost() {
+func HandleOnePost(payload string) {
+	// https://bitclout.com/posts/57d0a7d5640bdc2676df0e40f16f70393c4c484731a7867974c9c27789662ad8
+	hash := payload
+	if strings.HasPrefix(payload, "https") {
+		hash = payload[27:]
+	}
+	fmt.Println(hash)
+	p := cloutcli.SinglePost(hash)
+	fmt.Printf("%+v\n", p)
 }
